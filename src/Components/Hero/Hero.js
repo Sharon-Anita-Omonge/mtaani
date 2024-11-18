a1722a5af11559a81af65d7cbe07b68bfe383325
Sharon-Anita-Omonge <sharonanita2021@gmail.com>
2024-11-18 12:30:47 +0300

    h

--- src/Components/Hero/Hero.js
+++ src/Components/Hero/Hero.js
@@ -1,18 +1,19 @@
 import React from 'react';
-import './Hero.css'; 
+import './Hero.css';
 
 const Hero = () => {
-  return (
-    <div className="hero">
-      <div className="hero-content">
-        <h1>Maisha Mtaani, Changing Lives</h1>
-        <p>Welcome to Maisha Mtaani, a youth-led initiative dedicated to
-						empowering and uplifting young people and vulnerable individuals in
-						Kisumu.</p>
-      </div>
-     
-    </div>
-  );
+	return (
+		<div className="hero">
+			<div className="hero-content">
+				<h1>Maisha Mtaani, Changing Lives</h1>
+				<p>
+					Welcome to Maisha Mtaani, a youth-led initiative dedicated to
+					empowering and uplifting young people and vulnerable individuals in
+					Kisumu.
+				</p>
+			</div>
+		</div>
+	);
 };
 
 export default Hero;
