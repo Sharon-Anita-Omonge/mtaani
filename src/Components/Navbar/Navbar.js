9fe6e932a0d65c00c353b9f7935f00d54731b686
Sharon-Anita-Omonge <sharonanita2021@gmail.com>
2025-01-11 14:10:39 +0300

    nav

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,33 +1,48 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Navbar.css';
 import logo from '../../assets/logo.jpg';
 import { NavLink } from 'react-router-dom';
 
 const Navbar = () => {
+	const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+	const toggleMenu = () => {
+		setIsMenuOpen(!isMenuOpen);
+	};
+
 	return (
 		<nav className="navbar">
-			<NavLink to="/">
-				<img className="logo" src={logo} alt="Logo" />
-			</NavLink>
+			<div className="navbar-header">
+				<NavLink to="/">
+					<img className="logo" src={logo} alt="Logo" />
+				</NavLink>
+				<button className="hamburger" onClick={toggleMenu}>
+					<span className="bar"></span>
+					<span className="bar"></span>
+					<span className="bar"></span>
+				</button>
+			</div>
 
-			<ul className="navbar-links">
-				<li>
-					<a href="#home">Home</a>
-				</li>
-				<li>
-					<a href="#about">About Us</a>
-				</li>
-				<li>
-					<a href="#causes">Programs</a>
-				</li>
-				<li>
-					<a href="#volunteer">Volunteer</a>
-				</li>
-				<li>
-					<a href="#contact">Contact</a>
-				</li>
-			</ul>
-			<button className="navbar-donate">Donate</button>
+			<div className={`navbar-menu ${isMenuOpen ? 'open' : ''}`}>
+				<ul className="navbar-links">
+					<li>
+						<a href="#home">Home</a>
+					</li>
+					<li>
+						<a href="#about">About Us</a>
+					</li>
+					<li>
+						<a href="#causes">Programs</a>
+					</li>
+					<li>
+						<a href="#volunteer">Volunteer</a>
+					</li>
+					<li>
+						<a href="#contact">Contact</a>
+					</li>
+				</ul>
+				<button className="navbar-donate">Donate</button>
+			</div>
 		</nav>
 	);
 };
